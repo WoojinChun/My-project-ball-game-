3ef382eb53c559ba0dc984f3d53bb7af54a2f9f7
WoojinChun <woojinchun3@gmail.com>
2025-04-06 23:02:22 -0700

    Finished game
    finished game with jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+
+    private bool isGrounded = true;
+
+
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+    
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * 500);
+            isGrounded = false; // prevent further jumps until back on ground
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // If you hit the ground, re-enable jumping
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
 
     void OnMove(InputValue movementValue)
     {
@@ -21,10 +57,38 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >=12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed); 
     }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+        
+    }
+
+
 }
